5834e1a81ed626f37507fa052cea0c9d44b482d8
Anderson Dapper <anderson@undertech.poa.br>
2019-03-28 18:35:42 -0300

    Versão de teste

--- api/models/curriculosModel.js
+++ api/models/curriculosModel.js
@@ -1,34 +1,90 @@
 'use strict';
-var mongoose = require('mongoose');
+var 
+mongoose = require('mongoose'),
+b2a = require('b2a'),
+validator = require("email-validator");
+ 
+
+
+
 var Schema = mongoose.Schema;
+
 var curriculoSchema = new Schema({
    nome: {
-      type: String
+      type: String,
+      required: [true, 'Campo "nome" é obrigatório']
    },
    email: {
-      type: String
+      type: String,
+      validate: {
+         validator: function(v) {
+            return validator.validate(v);
+         },
+         message: props => `${props.value} não parece ser um email válido!`
+      },
+      required: [true, 'Campo "email" é obrigatório']
    },
    telefone: {
-       type: String
+      type: String,
+      validate: {
+         validator: function(v) {
+            return /\(\d{2}\)\d{4,5}-\d{4}/.test(v);
+         },
+         message: props => `${props.value} não parece ser um número válido! Utilize o formato (99)99999-9999`
+      },
+      required: [true, 'Campo "telefone" é obrigatório']      
    },
    linkedin: {
-       type: String
+       type: String,
+       required: [true, 'Campo "linkedin" é obrigatório']
    },
    pdfcurriculo : {
-      type: String
+      type: String,
+      validate: {
+         validator: function(v) {
+            try {
+            var teste = b2a.atob(v)
+            } catch(e) {
+             return false;
+            }
+            return true;
+         },
+         message: props => `${props.value} não parece ser um base64 válido!`
+      },
+      required: [true, 'Campo "pdfcurriculo" é obrigatório']
    },
    vaga : {
-      type: Number,
-      enum: [0,1],
-      default: 0
+      type: String,
+      enum: /*[0,1],*/
+      {
+         values: ['delphi', 'mobile']
+       , message: 'Campo "vaga" é obrigatório'
+       },
+      default: 'delphi'
    },
    ambiente : {
-      type: Number,
-      enum:[0,1],
-      default: 0
+      type: String,
+      enum: /*[0,1],*/
+      {
+         values: ['teste', 'producao']
+       , message: 'Campo "ambiente" é obrigatório'
+       },
+      default: 'teste'
    },
    zip: {
-      type: String
+      type: String,
+      validate: {
+         validator: function(v) {
+            try {
+               b2a.btoa(v);
+            } catch(e) {
+              return false;
+            }
+            return true;
+         },
+         message: props => `${props.value} não parece ser um base64 válido!`
+      },
+      required: [true, 'Campo "zip" é obrigatório']
    }
 });
 module.exports = mongoose.model('Curriculo', curriculoSchema);
--- server.js
+++ server.js
@@ -2,14 +2,26 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
-Curriculo = require('./api/models/curriculosModel'),
 bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/curriculodb');
+mongoose.connect('mongodb://localhost/curriculodb', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('view engine', 'pug');
+var model = require('./api/models/curriculosModel');
 var routes = require('./api/routes/curriculosRoute');
 routes(app);
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+  
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+  
 app.listen(port);
 console.log('Message RESTful API server started on: ' + port);
